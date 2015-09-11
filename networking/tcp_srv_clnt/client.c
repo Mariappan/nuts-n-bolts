c0c58aa3c4504653c24f78e35f31bbca8cb222de
Mariappan Ramasamy <rmariappan@outlook.com>
2015-09-11 16:54:42 +0530

    Fixed simple tcp server and client

--- networking/tcp_srv_clnt/client.c
+++ networking/tcp_srv_clnt/client.c
@@ -1,9 +1,9 @@
 /* Sample TCP client */
 
-#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
