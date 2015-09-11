425a0f6b7b9f7f2b4c17e28e0012f5c02f0b03f3
Mariappan Ramasamy <rmariappan@outlook.com>
2015-09-11 16:41:20 +0530

    Fixed simple server and client.

--- networking/simple_server_client1/client.c
+++ networking/simple_server_client1/client.c
@@ -8,52 +8,104 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/un.h>
+#include <errno.h>
 
 int main(){
   int clientSocket;
   char buffer[1024];
   socklen_t addr_size;
-  char *socket_path = "./socket";
+  int recvbytes;
+
+#ifdef SOCK_UNIX
+  char *socket_path = "\0socket";
   struct sockaddr_un serverAddr;
 
   memset(&serverAddr, 0, sizeof(serverAddr));
   serverAddr.sun_family = AF_UNIX;
   strncpy(serverAddr.sun_path, socket_path, sizeof(serverAddr.sun_path)-1);
-  unlink(socket_path);
 
-  clientSocket = socket(PF_UNIX, SOCK_STREAM, 0);
+  clientSocket = socket(AF_UNIX, SOCK_STREAM, 0);
+  printf ("Using UNIX socket:%d\n", clientSocket);
+  int on = 1;
+  if (-1 == setsockopt(clientSocket, IPPROTO_IP, IP_PKTINFO,(char *)&on, sizeof on)) {
+
+      perror("sesockopt error");
+      printf("Error no is %d\n", errno);
+  }
+#else
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(40899);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  printf ("Using INET socket:%d\n", clientSocket);
+
+  int on = 1;
+  if (-1 == setsockopt(clientSocket, IPPROTO_IP, IP_PKTINFO,(char *)&on, sizeof on)) {
+      perror("sesockopt error");
+      printf("Error no is %d\n", errno);
+  }
+#endif
 
   addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+  if (-1 == connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size)) {
+      perror ("Connect error\n");
+  }
 
+#if 1
   {
       struct msghdr child_msg;
-      int rc;
+      struct iovec iov[1];
+      char buf[1000];
 
       memset(&child_msg,   0, sizeof(child_msg));
-      char cmsgbuf[CMSG_SPACE(sizeof(int))];
+
+      child_msg.msg_iov = iov; /* vector for scatter read */
+      child_msg.msg_iovlen = 1; /* one buffer for scatter read */
+      iov[0].iov_base = buf; /* data we receive from the peer */
+      iov[0].iov_len = sizeof buf;
+
+      char cmsgbuf[CMSG_SPACE(1000)];
+
       child_msg.msg_control = cmsgbuf; // make place for the ancillary message to be received
       child_msg.msg_controllen = sizeof(cmsgbuf);
 
       printf("Waiting on recvmsg\n");
-      rc = recvmsg(clientSocket, &child_msg, 0);
-      printf("recvmsg return:%d\n", rc);
+      recvbytes = recvmsg(clientSocket, &child_msg, 0);
+      printf("recvmsg return:%d\n", recvbytes);
 
-      struct cmsghdr *cmsg = CMSG_FIRSTHDR(&child_msg);
-      printf("Cmsg is %p Msg_type:%d.\n", cmsg, cmsg?cmsg->cmsg_type:0);
-      if (cmsg == NULL) {
-          printf("The first control structure contains no file descriptor.\n");
-      }
+
+      if (recvbytes == -1)
+          printf("Error no is %d\n", errno);
       else
+          printf("recvbytes: %d\n", recvbytes);
+
+      if (recvbytes >= 0)
       {
-          int pass_sd;
-          memcpy(&pass_sd, CMSG_DATA(cmsg), sizeof(pass_sd));
-          printf("Received descriptor = %u %u\n", pass_sd, htons(pass_sd));
+          struct cmsghdr *cmsg = CMSG_FIRSTHDR(&child_msg);
+          printf("Cmsg is %p Msg_type:%d.\n", cmsg, cmsg?cmsg->cmsg_type:0);
+          if (cmsg == NULL) {
+              printf("The first control structure contains no file descriptor.\n");
+          }
+          else
+          {
+              int pass_sd;
+              memcpy(&pass_sd, CMSG_DATA(cmsg), sizeof(pass_sd));
+              printf("Received descriptor = %u %u\n", pass_sd, htons(pass_sd));
+          }
+
+          printf("Recieved data thro recvmsg is %s\n", buf);
       }
   }
 
-  recv(clientSocket, buffer, 1024, 0);
-  printf("Data received: %s",buffer);
+#else
+
+  recvbytes = recv(clientSocket, buffer, 1024, 0);
+  printf("Data received: %d %s", recvbytes, buffer);
+
+#endif
 
   close(clientSocket);
   return 0;
--- networking/simple_server_client1/server.c
+++ networking/simple_server_client1/server.c
@@ -15,18 +15,41 @@ int main(){
   char buffer[1024];
   struct sockaddr_storage serverStorage;
   socklen_t addr_size;
-  char *socket_path = "./socket";
+
+#ifdef SOCK_UNIX
+  char *socket_path = "\0socket";
   struct sockaddr_un serverAddr;
 
   memset(&serverAddr, 0, sizeof(serverAddr));
   serverAddr.sun_family = AF_UNIX;
   strncpy(serverAddr.sun_path, socket_path, sizeof(serverAddr.sun_path)-1);
-  unlink(socket_path);
 
+  printf ("Using UNIX socket\n");
+  welcomeSocket = socket(AF_UNIX, SOCK_STREAM, 0);
+#else
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(40899);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  welcomeSocket = socket(PF_UNIX, SOCK_STREAM, 0);
+  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+  printf ("Using INET socket:%d\n", welcomeSocket);
 
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+  int on = 1;
+  if (-1 == setsockopt(welcomeSocket, SOL_SOCKET, SO_REUSEADDR,(char *)&on, sizeof on)) {
+      perror("sesockopt SO_REUSEADDR error");
+  }
+
+  if (-1 == setsockopt(welcomeSocket, IPPROTO_IP, IP_PKTINFO,(char *)&on, sizeof on)) {
+      perror("sesockopt IP_PKTINFO error");
+  }
+
+#endif
+
+  if (bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) == -1) {
+      perror("bind error");
+  }
 
   if(listen(welcomeSocket,5)==0)
     printf("Listening\n");
@@ -36,20 +59,41 @@ int main(){
   /*---- Accept call creates a new socket for the incoming connection ----*/
   addr_size = sizeof serverStorage;
   newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+
+#ifndef SOCK_UNIX
+  if (-1 == setsockopt(newSocket, IPPROTO_IP, IP_PKTINFO,(char *)&on, sizeof on)) {
+      perror("sesockopt IP_PKTINFO error");
+  }
+#endif
+
+#if 1
   {
       struct msghdr parent_msg;
-      size_t length = 4089;
+      size_t length = 40;
+      struct iovec iov[1];
+      char buf[1000];
 
       memset(&parent_msg, 0, sizeof(parent_msg));
+      parent_msg.msg_iov = iov; /* vector for scatter read */
+      parent_msg.msg_iovlen = 1; /* one buffer for scatter read */
+      iov[0].iov_base = buf; /* data we receive from the peer */
+      iov[0].iov_len = sizeof buf;
+
+      strcpy(buf, "Hi Mari\n");
+      iov[0].iov_len = strlen(buf);
+
       struct cmsghdr *cmsg;
-      char cmsgbuf[CMSG_SPACE(sizeof(newSocket))];
+      char cmsgbuf[CMSG_SPACE(1000)];
+
       parent_msg.msg_control = cmsgbuf;
       parent_msg.msg_controllen = sizeof(cmsgbuf);
+
       cmsg = CMSG_FIRSTHDR(&parent_msg);
-      // cmsg->cmsg_level = SOL_SOCKET;
-      // cmsg->cmsg_type = SCM_RIGHTS;
-      cmsg->cmsg_len = CMSG_LEN(sizeof(newSocket));
+      cmsg->cmsg_level = IPPROTO_IP;
+      cmsg->cmsg_type = IP_TTL;
+      cmsg->cmsg_len = CMSG_LEN(sizeof(length));
       memcpy(CMSG_DATA(cmsg), &length, sizeof(length));
+
       parent_msg.msg_controllen = cmsg->cmsg_len; // total size of all control blocks
 
       if((sendmsg(newSocket, &parent_msg, 0)) < 0)
@@ -58,11 +102,13 @@ int main(){
           exit(0);
       }
   }
+#else
 
   /*---- Send message to the socket of the incoming connection ----*/
-  int opt = 0;
   strcpy(buffer,"Hello World\n");
   send(newSocket,buffer,13,0);
+
+#endif
 
   close(newSocket);
   close(welcomeSocket);
